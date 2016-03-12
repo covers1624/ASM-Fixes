2472643c6d6d86cdfa007beb69f2a879ed308e7c
covers1624 <laughlan.cov@internode.on.net>
2016-03-12 18:59:24 +1030

    Only run potion asm on Client.
    Should work on server now.

--- src/main/java/covers1624/asmfixes/asm/ASMTransformer.java
+++ src/main/java/covers1624/asmfixes/asm/ASMTransformer.java
@@ -6,6 +6,8 @@ import codechicken.lib.asm.ModularASMTransformer;
 import codechicken.lib.asm.ModularASMTransformer.MethodInjector;
 import codechicken.lib.asm.ModularASMTransformer.MethodReplacer;
 import codechicken.lib.asm.ObfMapping;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.launchwrapper.Launch;
 
@@ -31,48 +33,17 @@ public class ASMTransformer implements IClassTransformer {
         }
         asmBlocks = ASMReader.loadResource(clazz.getResourceAsStream("/blocks.asm"), "/blocks.asm");
 
-        //for (Map.Entry<String, ASMBlock> entry : asmBlocks.entrySet()) {
-        //    FMLLog.info("Block: %s", entry.getKey());
-        //    FMLLog.info("Instructions: \n %s", ASMHelper.toString(entry.getValue().list.list));
-        //}
-
-        transformer.add(new MethodInjector(new ObfMapping("net/minecraft/potion/PotionEffect", "func_76455_a", "(Lnet/minecraft/entity/EntityLivingBase;)Z"), asmBlocks.get("potionFix"), true));
+        if (FMLCommonHandler.instance().getSide() == Side.CLIENT) {
+            transformer.add(new MethodInjector(new ObfMapping("net/minecraft/potion/PotionEffect", "func_76455_a", "(Lnet/minecraft/entity/EntityLivingBase;)Z"), asmBlocks.get("potionFix"), true));
+        }
         transformer.add(new MethodReplacer(new ObfMapping("net/machinemuse/numina/recipe/SimpleItemMaker", "getRecipeOutput", "()Lnet/minecraft/item/ItemStack;"), asmBlocks.get("n_museLogSpam"), new ASMBlock()));
         transformer.add(new MethodReplacer(new ObfMapping("net/machinemuse/numina/recipe/SimpleItemMatcher", "matchesItem", "(Lnet/minecraft/item/ItemStack;)Z"), asmBlocks.get("n_museLogSpam"), new ASMBlock()));
     }
 
     @Override
     public byte[] transform(String name, String transformedName, byte[] bytes) {
-        //if (name.equals("net.machinemuse.numina.recipe.SimpleItemMatcher")) {
-        //    ClassNode classNode = new ClassNode();
-        //    ClassReader reader = new ClassReader(bytes);
-        //    reader.accept(classNode, 8);
-        //    for (MethodNode node : classNode.methods) {
-        //        FMLLog.info("Name: [%s], Desc: [%s].", node.name, node.desc);
-        //    }
-        //    MethodNode methodNode = ASMHelper.findMethod(new ObfMapping("net/machinemuse/numina/recipe/SimpleItemMatcher", "matchesItem", "(Lnet/minecraft/item/ItemStack;)Z"), classNode);
-        //    if (methodNode == null) {
-        //        throw new RuntimeException("Null method node.");
-        //    }
-        //    FMLLog.info("SimpleItemMaker.getRecipeOutput Instruction List:\n" + ASMHelper.toString(methodNode.instructions));
-        //}
+
         bytes = transformer.transform(name, bytes);
-        //if (name.equals("buildcraft.core.DefaultProps")) {
-        //ClassNode classNode = new ClassNode();
-        //ClassReader reader = new ClassReader(bytes);
-        //reader.accept(classNode, 8);
-        //for (MethodNode node : classNode.methods) {
-        //    FMLLog.info("Name: [%s], Desc: [%s].", node.name, node.desc);
-        //}
-        //for (FieldNode node : classNode.fields){
-        //    FMLLog.info("Name [%s], Value [%s]", node.name, node.value);
-        //}
-        //MethodNode methodNode = ASMHelper.findMethod(new ObfMapping("net/machinemuse/numina/recipe/SimpleItemMatcher", "matchesItem", "(Lnet/minecraft/item/ItemStack;)Z"), classNode);
-        // if (methodNode == null) {
-        //     throw new RuntimeException("Null method node.");
-        // }
-        //FMLLog.info("SimpleItemMaker.getRecipeOutput Instruction List:\n" + ASMHelper.toString(methodNode.instructions));
-        //}
 
         return bytes;
     }
