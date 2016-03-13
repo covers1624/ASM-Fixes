2f4028341d906c0cf806ea2dfaedf3d6771b0b54
covers1624 <laughlan.cov@internode.on.net>
2016-03-13 18:22:12 +1030

    Fix server side crashes.

--- src/main/java/covers1624/asmfixes/asm/ASMTransformer.java
+++ src/main/java/covers1624/asmfixes/asm/ASMTransformer.java
@@ -6,8 +6,8 @@ import codechicken.lib.asm.ModularASMTransformer;
 import codechicken.lib.asm.ModularASMTransformer.MethodInjector;
 import codechicken.lib.asm.ModularASMTransformer.MethodReplacer;
 import codechicken.lib.asm.ObfMapping;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.common.FMLLog;
+import cpw.mods.fml.relauncher.FMLLaunchHandler;
 import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.launchwrapper.Launch;
 
@@ -17,9 +17,10 @@ import java.util.Map;
  * Created by covers1624 on 3/7/2016.
  */
 public class ASMTransformer implements IClassTransformer {
-    private ModularASMTransformer transformer = new ModularASMTransformer();
+    private ModularASMTransformer commonTransformer = new ModularASMTransformer();
+    private ModularASMTransformer clientTransformer = new ModularASMTransformer();
+    private boolean hasInit = false;
     private Map<String, ASMBlock> asmBlocks;
-    private static String potionEffectClass = "net/minecraft/potion/PotionEffect";
 
     public ASMTransformer() {
         Class clazz;
@@ -32,19 +33,36 @@ public class ASMTransformer implements IClassTransformer {
             throw new RuntimeException(e);
         }
         asmBlocks = ASMReader.loadResource(clazz.getResourceAsStream("/blocks.asm"), "/blocks.asm");
-
-        if (FMLCommonHandler.instance().getSide() == Side.CLIENT) {
-            transformer.add(new MethodInjector(new ObfMapping("net/minecraft/potion/PotionEffect", "func_76455_a", "(Lnet/minecraft/entity/EntityLivingBase;)Z"), asmBlocks.get("potionFix"), true));
-        }
-        transformer.add(new MethodReplacer(new ObfMapping("net/machinemuse/numina/recipe/SimpleItemMaker", "getRecipeOutput", "()Lnet/minecraft/item/ItemStack;"), asmBlocks.get("n_museLogSpam"), new ASMBlock()));
-        transformer.add(new MethodReplacer(new ObfMapping("net/machinemuse/numina/recipe/SimpleItemMatcher", "matchesItem", "(Lnet/minecraft/item/ItemStack;)Z"), asmBlocks.get("n_museLogSpam"), new ASMBlock()));
     }
 
     @Override
     public byte[] transform(String name, String transformedName, byte[] bytes) {
+        if (!hasInit) {
+            initCommonTransformers();
+            if (FMLLaunchHandler.side().isClient()) {
+                initClientTransformers();
+            }
+            hasInit = true;
+        }
 
-        bytes = transformer.transform(name, bytes);
+        if (FMLLaunchHandler.side().isClient()) {
+            bytes = clientTransformer.transform(name, bytes);
+        }
+
+        bytes = commonTransformer.transform(name, bytes);
 
         return bytes;
     }
+
+    private void initClientTransformers() {
+        clientTransformer.add(new MethodInjector(new ObfMapping("net/minecraft/potion/PotionEffect", "func_76455_a", "(Lnet/minecraft/entity/EntityLivingBase;)Z"), asmBlocks.get("potionFix"), true));
+        FMLLog.info("Loading Client Transformers.");
+    }
+
+    private void initCommonTransformers() {
+        commonTransformer.add(new MethodReplacer(new ObfMapping("net/machinemuse/numina/recipe/SimpleItemMaker", "getRecipeOutput", "()Lnet/minecraft/item/ItemStack;"), asmBlocks.get("n_museLogSpam"), new ASMBlock()));
+        commonTransformer.add(new MethodReplacer(new ObfMapping("net/machinemuse/numina/recipe/SimpleItemMatcher", "matchesItem", "(Lnet/minecraft/item/ItemStack;)Z"), asmBlocks.get("n_museLogSpam"), new ASMBlock()));
+        FMLLog.info("Loading Common Transformers.");
+    }
+
 }
---
+++ src/main/java/covers1624/asmfixes/config/Config.java
@@ -0,0 +1,7 @@
+package covers1624.asmfixes.config;
+
+/**
+ * Created by covers1624 on 3/13/2016.
+ */
+public class Config {
+}
